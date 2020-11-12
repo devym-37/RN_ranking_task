acda03a8e19869399a43ac3406087975a8517c6c
Dev-miniyoung <miniyoung37@gmail.com>
2020-11-12 16:55:46 +0900

    udpate RankingVideo component

--- project/src/screens/RankingVideo/RankingVideoContainer.js
+++ project/src/screens/RankingVideo/RankingVideoContainer.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
 import RankingVideoPresenter from '~/screens/RankingVideo/RankingVideoPresenter';
 
+import RankingVideo from '~/popularVideoData';
+
 class RankingVideoContainer extends Component {
+    state = {
+        rankingVideo: []
+    }
+
+    componentDidMount(){
+        const fetchData = RankingVideo;
+        this.setState({
+            rankingVideo: fetchData
+        })
+    }
 
     render(){
+        const { rankingVideo } = this.state;
+
         return (
-            <RankingVideoPresenter />
+            <RankingVideoPresenter data={ rankingVideo } />
         )
     }
-}
+};
 
 export default RankingVideoContainer;
