f280f575fa9207b10df0dc4b39190b889b59625a
Dev-miniyoung <miniyoung37@gmail.com>
2020-11-12 17:02:46 +0900

    update RankingVideo components & json image url

--- project/src/screens/RankingVideo/RankingVideoPresenter.js
+++ project/src/screens/RankingVideo/RankingVideoPresenter.js
@@ -1,16 +1,22 @@
-import React from 'react'
+import React from 'react';
 import {
     Text,
     View,
-    StyleSheet
+    Image,
+    StyleSheet,
+    ScrollView
 } from 'react-native';
 
+import VideoCard from '~/components/VideoCard';
 
 
-const RankingVideoPresenter = () => {
+const RankingVideoPresenter = ({ data }) => {
     return ( 
         <View style = { styles.container } >
-            <Text>RankingVideoPresenter</Text>
+            <Text>{console.log('FakeData', data)}</Text>
+            <ScrollView showsVerticalScrollIndicator={false}>
+                { data.length !== 0 ? data.map((item, index) => <VideoCard key={index} data={item} index={index} /> ) : null}
+            </ScrollView>
         </View > 
         );
 };
@@ -18,8 +24,8 @@ const RankingVideoPresenter = () => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center"
+        alignItems: "center",
+        backgroundColor: "#FFF"
     },
 })
 
